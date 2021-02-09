96491ad0b26d0020354c13edf2da1dd7d67d8a1d
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-02-09 05:55:27 +0000

    added coach-list.vue and styling

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,34 @@
+<template>
+	<li>
+		<h3>{{ fullName }}</h3>
+		<h4>${{ rate }}/hour</h4>
+		<div>
+			<span v-for="area in areas" :key="area">{{ area }}</span>
+		</div>
+		<div class="actions">
+			<router-link :to="coachContactLink">Contact</router-link>
+			<router-link :to="coachDetailsLink">View Details</router-link>
+		</div>
+	</li>
+</template>
+
+<script>
+	export default {
+		props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+		computed: {
+			fullName(){
+				return this.$route.path + this.firstName + ' ' + this.lastName;
+			},
+			coachContactLink(){
+				return this.$route.path + '/coaches/' + this.id + '/contact'
+			},
+			coachDetailsLink(){
+				return this.$route.path + '/coaches/' + this.id;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	@import './coach-item.css'
+</style>

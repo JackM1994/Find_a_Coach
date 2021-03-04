ea8f57d2a1fea544376dff6dcda592ae3245a486
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-04 06:51:01 +0000

    added transitions between routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<the-header></the-header>
-	<router-view></router-view>
+	<router-view v-slot="slotProps">
+		<transition name="route" mode="out-in">
+			<component :is="slotProps.Component"></component>
+		</transition>
+	</router-view>
 </template>
 
 <script>
@@ -14,5 +18,26 @@ export default {
 </script>
 
 <style>
+	.route-enter-from{
+		opacity: 0;
+		transform: translateY(-30px);
+	}
+	.route-leave-to{
+		opacity: 0;
+		transform: translateY(30px);
+	}
+	.route-enter-active{
+		transform: all 0.3s ease-in;
+	}
+	.route-leave-active{
+		transform: all 0.3s ease-out;
+	}
+	.route-enter-to,
+	.route-leave-fron{
+		opacity: 1;
+		transform: translateY(0);
+	}
 	@import './app.css';
+
+
 </style>
--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -1,21 +1,24 @@
 <template>
 	<telport to="body">
 		<div v-if="show" @click="tryClose" class="backdrop">
-			<dialog open v-if="show">
-				<header>
-					<slot name="header">
-						<h2>{{ title }}</h2>
-					</slot>
-				</header>
-				<section>
-					<slot></slot>
-				</section>
-				<menu v-if="!fixed">
-					<slot name="actions">
-						<base-button @click="tryClose">Close</base-button>
-					</slot>
-				</menu>
-			</dialog>
+			<transition name="dialog">
+				<dialog open v-if="show">
+					<header>
+						<slot name="header">
+							<h2>{{ title }}</h2>
+						</slot>
+					</header>
+					<section>
+						<slot></slot>
+					</section>
+					<menu v-if="!fixed">
+						<slot name="actions">
+							<base-button @click="tryClose">Close</base-button>
+						</slot>
+					</menu>
+				</dialog>
+			</transition>
+			
 		</div>
 	</telport>
 </template>
@@ -92,6 +95,26 @@
 		justify-content: flex-end;
 		margin: 0;
 	}
+	.dialog-enter-from,
+	.dialog-leave-to{
+		opacity: 0;
+		transform: scale(0.8);
+	}
+
+	.dialog-enter-active{
+		transition: all 0.3s ease-out;
+	}
+
+	.dialog-leave-active{
+		transition: all 0.3s ease-in;
+	}
+
+	.dialog-enter-to,
+	.dialog-leave-from{
+		opacity: 1;
+		transform: scale(1);
+	}
+
 	@media (min-width: 768px){
 		dialog{
 			left: calc(50% - 20rem);

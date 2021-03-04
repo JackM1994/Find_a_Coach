b7aac6e3c1b024317de8e137907b9e727bd13673
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-04 06:56:45 +0000

    Added notFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+	<section>
+		<base-card>
+			<h2>Page not found!</h2>
+			<p>The page could not be found - maybe check out all our <router-link to='/coaches'>coaches</router-link></p>
+		</base-card>
+	</section>
+	
+</template>

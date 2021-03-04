6eb0efec2794184c8b7fd236ea6818c0fbcf8001
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-04 06:32:37 +0000

    caching http response data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -25,7 +25,11 @@ export default{
 			id: userId
 		});
 	},
-	async loadCoaches(context){
+	async loadCoaches(context, payload){
+		if(!payload.forceRefresh && !context.getters.shouldUpdate){
+			return;
+		}
+
 		const response =  await  fetch(`https://coach-app-e23d3-default-rtdb.firebaseio.com/coaches.json`
 		);
 	const responseData = await response.json();
@@ -46,5 +50,6 @@ export default{
 		coaches.push(coach);
 	}
 	context.commit('setCoaches', coaches);
+	context.commit('setFetchTimestamp');
 	}
 };
